4c150e106ca0d2f186288fa539f6c18dfbdef64d
walt <wwaltb@proton.me>
2024-04-22 23:04:18 -0700

    heic_to_pdf works on arch

--- neovim/.config/nvim/lua/plugins/lsp.lua
+++ neovim/.config/nvim/lua/plugins/lsp.lua
@@ -7,7 +7,7 @@ return {
       "williamboman/mason-lspconfig.nvim",
       config = function()
          require("mason-lspconfig").setup({
-            ensure_installed = { "lua_ls" },
+            ensure_installed = { "lua_ls", "bashls" },
          })
       end,
    },
