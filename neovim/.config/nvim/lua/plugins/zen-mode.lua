91fec8e84b9755574b91348773b0bbaf4076538d
wwaltb <44916811+wwaltb@users.noreply.github.com>
2024-04-26 17:49:56 -0700

    neovim stuff

---
+++ neovim/.config/nvim/lua/plugins/zen-mode.lua
@@ -0,0 +1,17 @@
+return {
+   {
+      "folke/twilight.nvim",
+      opts = {},
+   },
+   {
+      "folke/zen-mode.nvim",
+      opts = {
+         plugins = {
+            alacritty = {
+               enabled = true,
+               font = "14",
+            },
+         },
+      },
+   },
+}
