faf639e2ac4faeb47ab74aaffb14918cb31658bf
walt <wwaltb@proton.me>
2024-05-10 18:06:53 -0700

    new run pandoc command

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,4 +1,4 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    neofetch
+    # neofetch
 end
--- neovim/.config/nvim/lua/wb/mappings.lua
+++ neovim/.config/nvim/lua/wb/mappings.lua
@@ -43,4 +43,4 @@ vim.keymap.set("n", "<leader>zm", zen_mode.toggle, {})
 
 -- pandoc
 local run_pandoc = require("run-pandoc")
-vim.keymap.set("n", "<leader>cp", run_pandoc.run, {})
+vim.keymap.set("n", "<leader>cp", run_pandoc.compile, {})
