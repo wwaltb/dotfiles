c5a47801798b04201af871df9c1c695ddd4a43b8
wwaltb <44916811+wwaltb@users.noreply.github.com>
2024-06-09 20:54:01 -0700

    mini ai, pair and surround

---
+++ neovim/.config/nvim/lua/plugins/mini.lua
@@ -0,0 +1,14 @@
+return {
+	{
+		"echasnovski/mini.ai",
+        config = true,
+	},
+    {
+        'echasnovski/mini.pairs',
+        config = true,
+    },
+    {
+        'echasnovski/mini.surround',
+        config = true,
+    },
+}
