38a84e868b7b30c1ef6ea5f6afd85ef182659270
wwaltb <44916811+wwaltb@users.noreply.github.com>
2024-06-11 21:01:32 -0700

    show hidden in neo-tree

--- neovim/.config/nvim/lua/plugins/neo-tree.lua
+++ neovim/.config/nvim/lua/plugins/neo-tree.lua
@@ -9,6 +9,18 @@ return {
 	},
 	config = function()
 		require("neo-tree").setup({
+			filesystem = {
+				filtered_items = {
+					visible = true,
+					hide_dotfiles = false,
+					hide_gitignored = true,
+					hide_by_name = {
+						-- '.git',
+						-- '.DS_Store',
+						-- 'thumbs.db',
+					},
+				},
+			},
 			event_handlers = {
 				{
 					event = "file_opened",
