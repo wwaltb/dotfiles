87fd9bce750885b7f6b28e690a8e071a10ebe450
wwaltb <wwaltb@proton.me>
2025-01-16 10:55:06 -0800

    mini status line, files and picker. snacks zen mode and lazydev

--- neovim/.config/nvim/lua/plugins/mini.lua
+++ neovim/.config/nvim/lua/plugins/mini.lua
@@ -3,12 +3,19 @@ return {
         "echasnovski/mini.nvim",
         config = function()
             require("mini.ai").setup()
-            require("mini.files").setup()
+            require("mini.files").setup({
+                mappings = {
+                    close = "<Esc>",
+                }
+            })
             require("mini.pick").setup()
+            require("mini.extra").setup()
+            require("mini.git").setup()
             require("mini.diff").setup()
             require("mini.icons").setup()
             require("mini.pairs").setup()
             require("mini.surround").setup()
+            require("mini.statusline").setup()
         end,
     },
 }
